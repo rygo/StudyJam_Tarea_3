f4336994beaacfa4062e4a1c682fc72f7d006ba4
Guery Castaño Apaza <guerycastano@gmail.com>
2016-12-15 15:08:40 -0400

    tarea 3

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-Ejemplo Básico Vistas II (Views)
+TAREA 3
 ========================
+CALCULADORA
+===
 
-Esta aplicacion es un ejemplo basico de vistas en Android, llevado a cabo dentro del contenido del **Study Jam Android Development for Beginners** por el [GDG Android Bolivia.](http://www.gdg.androidbolivia.com/)
-
+se le aplico funcionalidad a la calculadora
 
 Requerimientos
 ------------
@@ -12,26 +13,18 @@ Requerimientos
   * minSdkVersion 15
 
 
-Uso
----------
-Para probar este ejemplo clona este repositorio de la siguiente forma:
->
->     $ git clone https://github.com/Gusn8/StudyJam_II_Views_II.git
-
-Dentro de Android Studio:
-
-* File --> New --> Import Project 
-* Seleccionas la ruta donde hiciste el `clone` del proyecto.
-* Build --> Rebuild Project
-* Run 
-
 Captura
 ---------
 
 <div align="center">
+    <br>   captura </br>
     <center>
-        <img src="/img/screenshot.jpg" width="300">
+        <img src="/img/imagen.png" width="300">
     </center>
+
+    <br> gif </br>
+            <img src="/img/calcu.gif" width="300">
+        </center>
 </div>
 <br><br>
 <a href="http://www.miramicodigo.com" target="_blank">@Gusn8</a>
--- app/src/main/java/com/miramicodigo/studyjam_ii_views_ii/MainActivity.java
+++ app/src/main/java/com/miramicodigo/studyjam_ii_views_ii/MainActivity.java
@@ -2,10 +2,22 @@ package com.miramicodigo.studyjam_ii_views_ii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultado;
+    private TextView Numero2;
+    private int can1=0;
+    private int can2;
+    private int res;
+    private double res2;
+    private int ope=0;
+    private int sw=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +26,292 @@ public class MainActivity extends AppCompatActivity {
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        resultado = (TextView) findViewById(R.id.tvResultado);
+
+    }
+    public void escribir1(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+1;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+1;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir2(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+2;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+2;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir3(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+3;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+3;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir4(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+4;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+4;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir5(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+5;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+5;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir6(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+6;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+6;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir7(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+7;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+7;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir8(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+8;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+8;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir9(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10+9;
+            resultado.setText(res+"");
+        }
+        else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10+9;
+            resultado.setText(res+"");
+        }
+    }
+    public  void escribir0(View w){
+        if(sw==1){
+            can1=0;
+            sw=0;
+            res = can1 * 10;
+            resultado.setText(res+"");
+        }else{
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 * 10;
+            resultado.setText(res+"");
+        }
+    }
+    public void borra(View w){
+        res = 0;
+        can1=0;
+        can2=0;
+        resultado.setText(res+"");
+    }
+    public void suma(View v){
+        if(can2>0){
+            if(ope==1){
+                can1 = Integer.parseInt(resultado.getText().toString());
+                res = can1 + can2;
+                resultado.setText(res+"");
+            }else{
+                if(ope==2){
+                    can1 = Integer.parseInt(resultado.getText().toString());
+                    res = can2 - can1;
+                    resultado.setText(res+"");
+                }else{
+                    if(ope==3){
+                        can1 = Integer.parseInt(resultado.getText().toString());
+                        res = can1 * can2;
+                        resultado.setText(res+"");
+                    }else{
+                        if(ope==4){
+                            can1 = Integer.parseInt(resultado.getText().toString());
+                            res2 = can1 / can2;
+                            resultado.setText(res+"");
+                        }
+                    }
+                }
+            }
+        }
+        sw=1;
+        can2 = Integer.parseInt(resultado.getText().toString());
+        ope=1;
+    }
+    public void resta(View v){
+        if(can2>0){
+            if(ope==1){
+                can1 = Integer.parseInt(resultado.getText().toString());
+                res = can1 + can2;
+                resultado.setText(res+"");
+            }else{
+                if(ope==2){
+                    can1 = Integer.parseInt(resultado.getText().toString());
+                    res = can2 - can1;
+                    resultado.setText(res+"");
+                }else{
+                    if(ope==3){
+                        can1 = Integer.parseInt(resultado.getText().toString());
+                        res = can1 * can2;
+                        resultado.setText(res+"");
+                    }else{
+                        if(ope==4){
+                            can1 = Integer.parseInt(resultado.getText().toString());
+                            res2 = can1 / can2;
+                            resultado.setText(res+"");
+                        }
+                    }
+                }
+            }
+        }
+        sw=1;
+        can2 = Integer.parseInt(resultado.getText().toString());
+        ope=2;
+    }
+    public void multi(View v){
+        if(can2>0){
+            if(ope==1){
+                can1 = Integer.parseInt(resultado.getText().toString());
+                res = can1 + can2;
+                resultado.setText(res+"");
+            }else{
+                if(ope==2){
+                    can1 = Integer.parseInt(resultado.getText().toString());
+                    res = can2 - can1;
+                    resultado.setText(res+"");
+                }else{
+                    if(ope==3){
+                        can1 = Integer.parseInt(resultado.getText().toString());
+                        res = can1 * can2;
+                        resultado.setText(res+"");
+                    }else{
+                        if(ope==4){
+                            can1 = Integer.parseInt(resultado.getText().toString());
+                            res2 = can1 / can2;
+                            resultado.setText(res+"");
+                        }
+                    }
+                }
+            }
+        }
+        can2 = Integer.parseInt(resultado.getText().toString());
+        ope=3;
+        sw=1;
+    }
+    public void divi(View v){
+        if(can2>0){
+            if(ope==1){
+                can1 = Integer.parseInt(resultado.getText().toString());
+                res = can1 + can2;
+                resultado.setText(res+"");
+            }else{
+                if(ope==2){
+                    can1 = Integer.parseInt(resultado.getText().toString());
+                    res = can2 - can1;
+                    resultado.setText(res+"");
+                }else{
+                    if(ope==3){
+                        can1 = Integer.parseInt(resultado.getText().toString());
+                        res = can1 * can2;
+                        resultado.setText(res+"");
+                    }else{
+                        if(ope==4){
+                            can1 = Integer.parseInt(resultado.getText().toString());
+                            res2 = can2 / can1;
+                            resultado.setText(res2+"");
+                        }
+                    }
+                }
+            }
+        }
+        can2 = Integer.parseInt(resultado.getText().toString());
+        ope=4;
+        sw=1;
+    }
+    public void igual(View w){
+        if(ope==1){
+            can1 = Integer.parseInt(resultado.getText().toString());
+            res = can1 + can2;
+            resultado.setText(res+"");
+        }else{
+            if(ope==2){
+                can1 = Integer.parseInt(resultado.getText().toString());
+                res = can2 - can1;
+                resultado.setText(res+"");
+            }else{
+                if(ope==3){
+                    can1 = Integer.parseInt(resultado.getText().toString());
+                    res = can1 * can2;
+                    resultado.setText(res+"");
+                }else{
+                    if(ope==4){
+                        can1 = Integer.parseInt(resultado.getText().toString());
+                        res2 = can2 / can1;
+                        resultado.setText(res2+"");
+                    }else{
+                        res = 0;
+                        resultado.setText(res+"");
+                    }
+                }
+            }
+        }
+        sw=1;
+        ope=0;
     }
 }
